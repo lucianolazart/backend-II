89931f743f8e1d0b3474dd9ecd98c428e826f052
lucianolazart <lucianolazart@gmail.com>
2025-08-07 22:04:40 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import {Server} from 'socket.io';
+import mongoose from 'mongoose';
+
+import productRouter from './routes/productRouter.js';
+import cartRouter from './routes/cartRouter.js';
+import viewsRouter from './routes/viewsRouter.js';
+import __dirname from './utils/constantsUtil.js';
+import websocket from './websocket.js';
+
+const app = express();
+
+const uri = 'mongodb://127.0.0.1:27017/entrega-final';
+mongoose.connect(uri);
+
+//Handlebars Config
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/../views');
+app.set('view engine', 'handlebars');
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+//Routers
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
+app.use('/', viewsRouter);
+
+const PORT = 8080;
+const httpServer = app.listen(PORT, () => {
+    console.log(`Start server in PORT ${PORT}`);
+});
+
+const io = new Server(httpServer);
+
+websocket(io);
